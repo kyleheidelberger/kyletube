f88030b13c13d12145caae6f36a57e9ee43d6362
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-04 16:23:17 -0400

    add related videos section

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,13 +1,10 @@
 import React from 'react';
-import {VideoPreview} from '../../components/VideoPreview/VideoPreview';
+import {RelatedVideos} from '../../components/RelatedVideos/RelatedVideos';
 
 export class Watch extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <VideoPreview horizontal={true}/>
-        <VideoPreview />
-      </React.Fragment>
+      <RelatedVideos/>
     );
   }
 }
