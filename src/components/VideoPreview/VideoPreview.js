c62ba41a6d37d6b03d20d6dc37b021cc88dc0d6d
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-04 16:16:58 -0400

    update video preview

--- src/components/VideoPreview/VideoPreview.js
+++ src/components/VideoPreview/VideoPreview.js
@@ -1,23 +1,26 @@
-import React from 'react';
-import {Image} from 'semantic-ui-react';
-import './VideoPreview.scss';
+import React from "react";
+import { Image } from "semantic-ui-react";
+import "./VideoPreview.scss";
 
 export class VideoPreview extends React.Component {
   render() {
+    const horizontal = this.props.horizontal ? "horizontal" : null;
     return (
-      <div className='video-preview'>
-        <div className='image-container'>
-          <Image src='http://via.placeholder.com/210x118'/>
-          <div className='time-label'>
+      <div className={["video-preview", horizontal].join(" ")}>
+        <div className="image-container">
+          <Image src="http://via.placeholder.com/210x118" />
+          <div className="time-label">
             <span>05:22</span>
           </div>
         </div>
 
-        <div className='video-info'>
-          <div className='semi-bold show-max-two-lines'>Video title</div>
-          <div className='video-preview-metadata-container'>
-            <div className='channel-title'>Channel title</div>
-            <div><span>2.1M views • 2 days ago</span></div>
+        <div className="video-info">
+          <div className="semi-bold show-max-two-lines">Video title</div>
+          <div className="video-preview-metadata-container">
+            <div className="channel-title">Channel title</div>
+            <div>
+              <span>2.1M views • 2 days ago</span>
+            </div>
           </div>
         </div>
       </div>
--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import {Video} from '../../components/Video/Video';
+import {VideoPreview} from '../../components/VideoPreview/VideoPreview';
 
 export class Watch extends React.Component {
   render() {
     return (
-      <div style={{maxWidth: '80%'}}>
-        <Video id='-7fuHEEmEjs'/>
-      </div>
+      <React.Fragment>
+        <VideoPreview horizontal={true}/>
+        <VideoPreview />
+      </React.Fragment>
     );
   }
 }
