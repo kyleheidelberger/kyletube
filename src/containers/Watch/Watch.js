b1744a8cde629564b838ac57abcc311bbb2df0e1
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-04 16:59:01 -0400

    add comments

--- src/containers/Watch/Watch.js
+++ src/containers/Watch/Watch.js
@@ -2,15 +2,18 @@ import React from 'react';
 import './Watch.scss';
 import {RelatedVideos} from '../../components/RelatedVideos/RelatedVideos';
 import {Video} from '../../components/Video/Video';
+import {VideoMetadata} from '../../components/VideoMetadata/VideoMetadata';
+import {VideoInfoBox} from '../../components/VideoInfoBox/VideoInfoBox';
+import {Comments} from '../Comments/Comments';
 
 export class Watch extends React.Component {
   render() {
     return (
       <div className='watch-grid'>
-        <Video className='video' id='-7fuHEEmEjs' />
-        <div className='metadata' style={{width: '100%', height: '100px', background: '#F91112'}}>Metadata</div>
-        <div className='video-info-box' style={{width: '100%', height: '100px', background: '#BD10E0'}}>Video Info box</div>
-        <div className='comments' style={{width: '100%', height: '100px', background: '#9013FE'}}>comments</div>
+        <Video className='video' id='-7fuHEEmEjs'/>
+        <VideoMetadata className='metadata' viewCount={1000}/>
+        <VideoInfoBox className='video-info-box'/>
+        <Comments className='comments'/>
         <RelatedVideos className='relatedVideos'/>
       </div>
     );
