a639d43e3efa4024e8f7e80ef96aba9771010ab6
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-04 16:01:32 -0400

    step 10

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import {SideBar} from '../SideBar/SideBar';
 import './Home.scss';
 import {VideoGrid} from '../../components/VideoGrid/VideoGrid';
-
+import {SideBar} from '../SideBar/SideBar';
 
 export class Home extends React.Component {
-    render() {
-      return (
-        <React.Fragment>
-          <SideBar/>
-          <div className='home'>
-            <div className="responsive-video-grid-container">
-              <VideoGrid title='Trending'/>
-              <VideoGrid title='Autos & Vehicles' hideDivider={true}/>
-            </div>
-          </div>
-        </React.Fragment>
-      );
-    }
+  render() {
+    return (
+      <React.Fragment>
+        <SideBar/>
+        <div className='home'>
+          <VideoGrid title='Trending'/>
+          <VideoGrid title='Autos & Vehicles' hideDivider={true}/>
+        </div>
+      </React.Fragment>
+    );
   }
+}
