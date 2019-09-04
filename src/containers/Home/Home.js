ce9561b98c663a73157bc82da8df120fa97efcb4
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-04 15:11:19 -0400

    add video grid tests

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import {VideoPreview} from '../../components/VideoPreview/VideoPreview';
+import {SideBar} from '../SideBar/SideBar';
 import './Home.scss';
+import {VideoGrid} from '../../components/VideoGrid/VideoGrid';
+
 
 export class Home extends React.Component {
-  render() {
-    return (
-      <div className='home'>
-        <VideoPreview />
-      </div>
-    );
+    render() {
+      return (
+        <React.Fragment>
+          <SideBar/>
+          <div className='home'>
+            <div className="responsive-video-grid-container">
+              <VideoGrid title='Trending'/>
+              <VideoGrid title='Autos & Vehicles' hideDivider={true}/>
+            </div>
+          </div>
+        </React.Fragment>
+      );
+    }
   }
-}
