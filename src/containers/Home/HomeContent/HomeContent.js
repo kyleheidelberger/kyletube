da311063deab11373fd641e24af5ab90de44f150
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-04 17:39:00 -0400

    add files to store directory

---
+++ src/containers/Home/HomeContent/HomeContent.js
@@ -0,0 +1,17 @@
+
+import {VideoGrid} from '../../../components/VideoGrid/VideoGrid';
+import React from 'react';
+import './HomeContent.scss';
+
+export class HomeContent extends React.Component {
+  render() {
+    return (
+      <div className='home-content'>
+        <div className="responsive-video-grid-container">
+          <VideoGrid title='Trending'/>
+          <VideoGrid title='Autos & Vehicles' hideDivider={true}/>
+        </div>
+      </div>
+    );
+  }
+}
