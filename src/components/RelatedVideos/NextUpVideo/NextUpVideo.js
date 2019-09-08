56038ffbb818aa5ac5841a3edfd2ba70f897a490
Kyle Heidelberger <kyle.heidelberger@gmail.com>
2019-09-08 19:56:25 -0400

    complete part 36 of tutorial

--- src/components/RelatedVideos/NextUpVideo/NextUpVideo.js
+++ src/components/RelatedVideos/NextUpVideo/NextUpVideo.js
@@ -10,10 +10,10 @@ export function NextUpVideo(props) {
           <h4>Up next</h4>
           <div className='up-next-toggle'>
             <span>Autoplay</span>
-            <Checkbox toggle defaultChecked/>
+            <Checkbox toggle defaultChecked />
           </div>
       </div>
-      <VideoPreview horizontal={true}/>
+      <VideoPreview horizontal={true} video={props.video} pathname='/watch' search={`?v=${props.video.id}`}/>
       <Divider/>
     </React.Fragment>
   );
